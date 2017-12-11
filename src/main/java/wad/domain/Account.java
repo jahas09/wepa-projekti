145f394aa2104be6fcac59494b25734fc7a5b5f9
Janne <janne.hanski@gmail.com>
2017-12-11 21:20:17 +0200

    travisin tiedosto, konfiguraatioita ja käyttäjä-luokka lisätty

---
+++ src/main/java/wad/domain/Account.java
@@ -0,0 +1,19 @@
+
+package wad.domain;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Account extends AbstractPersistable<Long>{
+    private String username;
+    private String password;
+}
